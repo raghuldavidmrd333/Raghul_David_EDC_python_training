7365468df45aaa782d23f966b76b84beb33b57b6
raghuldavidmrd333 <98683176+raghuldavidmrd333@users.noreply.github.com>
2023-10-04 09:26:41 +0530

    Add files via upload

---
+++ assignment9_2.py
@@ -0,0 +1,61 @@
+class Book:
+    def __init__(self,id,title,author,price,rating):
+        self.id=id
+        self.title=title
+        self.author=author
+        self.price=price 
+        self.rating=rating
+class library:
+    def __init__(self):
+        self.books=[]
+    def add_book(self,book):
+        self.books.append(book)
+    def Find_by_ID(self,ID):
+        for book in self.books:
+            if book.id==ID:
+                print(f"{book.id},{book.title},{book.author},{book.price},{book.rating}")
+        return None
+    def Find_by_author(self,author_name):
+        for book in self.books:
+            if book.author==author_name:
+                print(f"{book.id},{book.title},{book.author},{book.price},{book.rating}")
+        return None
+    def Find_by_rating(self,start,end):
+        for book in self.books:
+            if (book.rating>start and book.rating<end):
+                print(f"{book.id},{book.title},{book.author},{book.price},{book.rating}")
+        return None
+    def Find_by_price(self,start,end):
+        for book in self.books:
+            if (book.price>start and book.price<end):
+                print(f"{book.id},{book.title},{book.author},{book.price},{book.rating}")
+        return None
+    def info(self):
+        for i in self.books:
+            print(f"{i.id},{i.title},{i.author},{i.price},{i.rating}")
+
+b1=Book(1111,"chronicles of narnia","MRD",200,4)
+b2=Book(2222,"harrypotter","Raghul",100,5)
+b3=Book(3333,"marvel","SAI",250,3)
+b4=Book(4444,"GOT","PRAVEEN",400,2)
+l=library()
+l.add_book(b1)
+print()
+l.add_book(b2)
+l.add_book(b3)
+l.add_book(b4)
+print("Books list:")
+print()
+l.info()
+print("finding book by id=102:")
+print()
+l.Find_by_ID(102)
+print("finding book by author=MRD:")
+print()
+l.Find_by_author("MRD")
+print("finding books by rating between 3 and 5")
+print()
+l.Find_by_rating(2,6)
+print("finding book by price between 150 and 300")
+print()
+l.Find_by_price(200,350)
